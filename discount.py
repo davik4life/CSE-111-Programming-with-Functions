9d51b0cd4e8426471e68e2933438baadcc5d87c0
Victor Adeshile <davik4life@gmail.com>
2024-02-25 04:20:43 +0100

    Discount.py stretch challenge

--- discount.py
+++ discount.py
@@ -22,22 +22,39 @@ the sales tax amount, and the total amount due.
 
 current_date_and_time = datetime.now()
 day_of_week = current_date_and_time.weekday()
+reps = True
+while reps:
+    subtotal = float(input("Please enter the subtotal: "))
+    if subtotal == 0:
+        break
+    discount_amount = subtotal * 10 / 100
+    discounted_amount = subtotal * 90 / 100 
+    tax_amount = discounted_amount * 6 / 100
+    tax_amount_else = subtotal * 6 / 100
+    total =  round(discounted_amount + tax_amount, 2)
+    total_else =  round(subtotal + tax_amount_else, 2)
+    # total_with_discount =  round(tax_amount, 2)
 
-subtotal = float(input("Please enter the subtotal: "))
-discount_amount = subtotal * 10 / 100
-discounted_amount = subtotal * 90 / 100 
-tax_amount = discounted_amount * 6 / 100
-tax_amount_else = subtotal * 6 / 100
-total =  round(discounted_amount + tax_amount, 2)
-total_else =  round(subtotal + tax_amount_else, 2)
-total_with_discount =  round(tax_amount, 2)
-
-if  subtotal >= 50 and (day_of_week == 1 or day_of_week == 6):
-    print(f"Discount amount: {round(discount_amount, 2)}")
-    print(f"Sales tax amount: {round(tax_amount, 2)}")
-    print(f"Total: {round(total, 2)}")
-else:
-    print(f"Sales tax amount: {round(tax_amount_else, 2)}")
-    print(f"Total: {total_else}")
+    if  subtotal >= 50 and (day_of_week == 1 or day_of_week == 6):
+        print(f"Discount amount: {round(discount_amount, 2)}")
+        print(f"Sales tax amount: {round(tax_amount, 2)}")
+        print(f"Total: {total}")
+    elif subtotal < 50 and (day_of_week == 1 or day_of_week == 6):
+        print(f"You are not purchasing enough to recieve a discount. you need {50 - subtotal} worth of purchase to qualify for a discount.")
+        print(f"Sales tax amount: {round(tax_amount_else, 2)}")
+        print(f"Total: {total_else}")
+    else:
+        print(f"Sales tax amount: {round(tax_amount_else, 2)}")
+        print(f"Total: {total_else}")
+        
+    continue_purchase = input
+    
+    #Stretch Challenges Instructions
+    
+    """
+    Add code to your program that the computer will execute if today is Tuesday or Wednesday and the customer is not purchasing enough to receive the discount. This added code should compute and print the difference between $50 and the subtotal which is the additional amount the customer would need to purchase in order to receive the discount.
+    
+    Near the beginning of your program replace the code that asks the user for the subtotal with a loop that repeatedly asks the user for a price and a quantity and computes the subtotal. This loop should repeat until the user enters "0" for the price.
+    """
     
     
