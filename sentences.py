99454e6d102e328df9d18b756e621a9a4ddb6e6b
Victor Adeshile <davik4life@gmail.com>
2024-03-02 22:01:53 +0100

    This the building structure of sentences.py

---
+++ sentences.py
@@ -0,0 +1,26 @@
+def main():
+    """
+    This is the main app.
+    """
+    
+def make_sentence():
+    """
+    This is the function that makes sentence.
+    """
+
+def get_determiner():
+    """
+    This is the function that is the determiner.
+    """
+    
+def get_noun():
+    """
+    This is the function that gets the noun.
+    """
+    
+def get_verb():
+    """
+    This is the funtion that gets the verb.
+    """
+    
+main()
