df9cbf64a5b02dc23c763f5835528caa3a36ea43
Victor Adeshile <davik4life@gmail.com>
2024-03-03 21:53:39 +0100

    added the word variable with the list properties.

--- sentences.py
+++ sentences.py
@@ -1,8 +1,13 @@
+import random
+
 def main():
     """
     This is the main app.
     """
     
+    words = ["boy", "girl", "cat", "dog", "bird", "house"]
+    
+    words = random.choice(words)
 def make_sentence():
     """
     This is the function that makes sentence.
