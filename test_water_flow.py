a42964c99cb94c37f8c7d85c919ac867ecd5bbcf
Victor Adeshile <davik4life@gmail.com>
2024-03-11 04:30:37 +0100

    Water_flow prove and Strectch

--- test_water_flow.py
+++ test_water_flow.py
@@ -1,4 +1,4 @@
-from water_flow import water_column_height, pressure_gain_from_water_height, pressure_loss_from_pipe
+from water_flow import water_column_height, pressure_gain_from_water_height, pressure_loss_from_pipe, pressure_loss_from_fittings, reynolds_number, pressure_loss_from_pipe_reduction, converts_KPA_to_PSI
 from pytest import approx
 import pytest
 
@@ -22,4 +22,29 @@ def test_pressure_loss_from_pipe():
     assert pressure_loss_from_pipe(0.286870, 1000.00, 0.013, 1.65) == approx(-61.576, abs=0.001)
     assert pressure_loss_from_pipe(0.286870, 1800.75, 0.013, 1.65) == approx(-110.884, abs=0.001)
     
+def test_pressure_loss_from_fittings():
+    assert pressure_loss_from_fittings(0.00, 3) == approx(0.000, abs=0.001)
+    assert pressure_loss_from_fittings(1.65, 0) == approx(0.000, abs=0.001)
+    assert pressure_loss_from_fittings(1.65, 2) == approx(-0.109, abs=0.001)
+    assert pressure_loss_from_fittings(1.75, 2) == approx(-0.122, abs=0.001)
+    assert pressure_loss_from_fittings(1.75, 5) == approx(-0.306, abs=0.001)
+    
+def test_reynolds_number():
+    assert reynolds_number(0.048692, 0.00) == approx(0, abs=1)
+    assert reynolds_number(0.048692, 1.65) == approx(80069, abs=1)
+    assert reynolds_number(0.048692, 1.75) == approx(84922, abs=1)
+    assert reynolds_number(0.286870, 1.65) == approx(471729, abs=1)
+    assert reynolds_number(0.286870, 1.75) == approx(500318, abs=1)
+    
+def test_pressure_loss_from_pipe_reduction():
+    assert pressure_loss_from_pipe_reduction(0.28687, 0.00, 1, 0.048692) == approx(0.000, abs=0.001)
+    assert pressure_loss_from_pipe_reduction(0.28687, 1.65, 471729, 0.048692) == approx(-163.744, abs=0.001)
+    assert pressure_loss_from_pipe_reduction(0.28687, 1.75, 500318, 0.048692) == approx(-184.182, abs=0.001)
+    
+def test_converts_KPA_to_PSI():
+    assert converts_KPA_to_PSI(158.7) == approx(23.017489, abs=0.01)
+    assert converts_KPA_to_PSI(33) == approx(4.78625, abs=0.01)
+    assert converts_KPA_to_PSI(168) == approx(24.3663, abs=0.01)
+    assert converts_KPA_to_PSI(945) == approx(137.061, abs=0.01)
+
 pytest.main(["-v", "--tb=line", "-rN", "test_water_flow.py"])
--- water_flow.py
+++ water_flow.py
@@ -1,3 +1,7 @@
+EARTH_ACCELERATION_OF_GRAVITY = 9.8066500
+WATER_DENSITY = 998.2000000
+WATER_DYNAMIC_VISCOSITY	= 0.0010016
+PPSI = 6.895
 def water_column_height(tower_height, tank_height):
     """
     This is a function that returns the water column height.
@@ -10,9 +14,8 @@ def pressure_gain_from_water_height(height):
     """
     This is a function that returns the pressure gain from the water height.
     """
-    p = 998.2
-    g =  9.80665
-    pressure_gain = (p * g * height) / 1000
+   
+    pressure_gain = (WATER_DENSITY * EARTH_ACCELERATION_OF_GRAVITY * height) / 1000
     
     return pressure_gain
 
@@ -20,8 +23,98 @@ def pressure_loss_from_pipe(pipe_diameter, pipe_length, friction_factor, fluid_v
     """
     This is the function that returns the pressure loss from pipe.
     """
-    p = 998.2
     
-    pressure_loss = (-friction_factor * pipe_length * p) *  (fluid_velocity**2) / (2000*pipe_diameter)
+    pressure_loss = (-friction_factor * pipe_length * WATER_DENSITY) *  (fluid_velocity**2) / (2000*pipe_diameter)
     
     return pressure_loss
+
+def pressure_loss_from_fittings(fluid_velocity, quatity_fittings):
+    """
+    This is the function that calculates the pressure.
+
+    Args:
+        fluid_velocity (_type_): _description_
+        quatity_fittings (_type_): _description_
+    """
+    
+    pressure_loss = -0.04 * WATER_DENSITY * (fluid_velocity**2) * quatity_fittings/2000
+    return pressure_loss
+
+def reynolds_number(hydraulic_diameter, fluid_velocity):
+    """
+    This is the function that calculates the Reynolds number and returns the value.
+
+    Args:
+        hydraulic_diameter (_type_): _description_
+        fluid_velocity (_type_): _description_
+    """
+        
+    reynolds_num = WATER_DENSITY * hydraulic_diameter * fluid_velocity / WATER_DYNAMIC_VISCOSITY
+    return reynolds_num
+
+def pressure_loss_from_pipe_reduction(larger_diameter, fluid_velocity, reynolds_number, smaller_diameter):
+    """
+    This is the function that computes  the pressure reduction due to friction factor.
+    """
+    
+    constant_computed = (0.1 + 50 / reynolds_number) * (((larger_diameter/smaller_diameter) **4) -1)
+    pressure_loss = (-constant_computed * WATER_DENSITY *  (fluid_velocity ** 2))/2000
+    return pressure_loss
+
+def converts_KPA_to_PSI(kpa):
+    """
+    Converts KPA to PSI and returns the value.
+    """
+    
+    converted_KPA = kpa / PPSI
+    return converted_KPA
+
+
+    
+PVC_SCHED80_INNER_DIAMETER = 0.28687 # (meters)  11.294 inches
+PVC_SCHED80_FRICTION_FACTOR = 0.013  # (unitless)
+SUPPLY_VELOCITY = 1.65               # (meters / second)
+
+HDPE_SDR11_INNER_DIAMETER = 0.048692 # (meters)  1.917 inches
+HDPE_SDR11_FRICTION_FACTOR = 0.018   # (unitless)
+HOUSEHOLD_VELOCITY = 1.75            # (meters / second)
+
+
+def main():
+    tower_height = float(input("Height of water tower (meters): "))
+    tank_height = float(input("Height of water tank walls (meters): "))
+    length1 = float(input("Length of supply pipe from tank to lot (meters): "))
+    quantity_angles = int(input("Number of 90° angles in supply pipe: "))
+    length2 = float(input("Length of pipe from supply to house (meters): "))
+
+    water_height = water_column_height(tower_height, tank_height)
+    pressure = pressure_gain_from_water_height(water_height)
+
+    diameter = PVC_SCHED80_INNER_DIAMETER
+    friction = PVC_SCHED80_FRICTION_FACTOR
+    velocity = SUPPLY_VELOCITY
+    reynolds = reynolds_number(diameter, velocity)
+    loss = pressure_loss_from_pipe(diameter, length1, friction, velocity)
+    pressure += loss
+
+    loss = pressure_loss_from_fittings(velocity, quantity_angles)
+    pressure += loss
+
+    loss = pressure_loss_from_pipe_reduction(diameter,
+            velocity, reynolds, HDPE_SDR11_INNER_DIAMETER)
+    pressure += loss
+
+    diameter = HDPE_SDR11_INNER_DIAMETER
+    friction = HDPE_SDR11_FRICTION_FACTOR
+    velocity = HOUSEHOLD_VELOCITY
+    loss = pressure_loss_from_pipe(diameter, length2, friction, velocity)
+    pressure += loss
+    
+    psi_value = converts_KPA_to_PSI(pressure)
+    print(f"Pressure in PSI is: {psi_value} Pounds Per Square Inch.")
+    print()    
+    print(f"Pressure at house: {pressure:.1f} kilopascals")
+
+
+if __name__ == "__main__":
+    main()
