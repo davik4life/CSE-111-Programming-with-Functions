614272e1b47d4b80709bdc14fc976f3d4e799993
Victor Adeshile <davik4life@gmail.com>
2024-03-30 17:07:24 +0100

    Added the receipt.py file

--- receipt.py
+++ receipt.py
@@ -1,44 +1,82 @@
 import csv
+from datetime import datetime
 
 def main():
     """
     This is the main function.
     """
+    try:
     # Create a variable to store the csv file.
-    filename = "products.csv"
-    
-    # Create a constant variable to avoid directly hard-coding the parameter
-    KEY_INDEX = 0
-    PRODUCT_NAME_INDEX = 1
-    PRODUCT_PRICE_INDEX = 2
-    
-    # Create a variable that calls the read_dictionary function and stores it for later reference,
-    products_dict = read_dictionary(filename, KEY_INDEX)
-    
-    
-    print()
-    print("All Products")
-    print()
-    # Print the products_dict.
-    print(products_dict)
-    request_file = "request.csv"
-    
-    # Open the request.csv file for reading.
-    with open(request_file, "rt") as request_data:
-        reader = csv.reader(request_data)
+        filename = "products.csv"
         
-        # Skip the first line of the file.
-        next(reader)
+        # Create a constant variable to avoid directly hard-coding the parameter
+        KEY_INDEX = 0
+        PRODUCT_NAME_INDEX = 1
+        PRODUCT_PRICE_INDEX = 2
         
+        # Prove Assignment Additions
+        STORE_NAME = "Victorious Super-Store"
+        number_of_items = 0
+        subtotal = 0
+        SALES_TAX = 6
+        PERCENTAGE = 100
+        current_datetime = datetime.now()
+        
+        
+        # Create a variable that calls the read_dictionary function and stores it for later reference,
+        products_dict = read_dictionary(filename, KEY_INDEX)
+        
+        print(STORE_NAME)
         print()
-        print("Reqeusted Items")
-        for request in reader:
-            product_name, quantity = request
-             
-            if product_name in products_dict:
-                print(f"{products_dict[product_name][PRODUCT_NAME_INDEX]}: {quantity} @ {products_dict[product_name][PRODUCT_PRICE_INDEX]}")
+        # print("All Products")
+        # print()
+        # Print the products_dict.
+        # print(products_dict)
+        request_file = "request.csv"
+        
+        # Open the request.csv file for reading.
+        with open(request_file, "rt") as request_data:
+            reader = csv.reader(request_data)
+            
+            # Skip the first line of the file.
+            next(reader)
+            
+            print()
+            # print("Reqeusted Items")
+            for request in reader:
+                product_name, quantity = request
+                quantity_converted = int(quantity)
+                number_of_items += quantity_converted
+                
+                try:            
+                    if product_name in products_dict:
+                        price = products_dict[product_name][PRODUCT_PRICE_INDEX]
+                        name = products_dict[product_name][PRODUCT_NAME_INDEX]
+                        subtotal += quantity_converted * float(price)
+                        sales_tax = subtotal * SALES_TAX / PERCENTAGE
+                        total = sales_tax + subtotal
+                        print(f"{name}: {quantity} @ {price}")
+                    else:
+                        print(f"Product {product_name} not found.")
+                except KeyError:
+                    print("KeyError occured while accessing products dictionary.")
 
-    
+        print()
+        print(f"Number of Items: {number_of_items}")
+        print(f"Subtotal: {subtotal:.2f}")
+        print(f"Sales Tax: {sales_tax:.2f}")
+        print(f"Total: {total:.2f}")
+        print()
+        print(f"Thank you for shopping at the {STORE_NAME}.")
+        print(f"{current_datetime:%c }")
+    except FileNotFoundError:
+        print(f"File '{filename}' not found.")
+    except PermissionError:
+        print(f"Permission denied to access '{filename}'.")
+    except Exception as e:
+        print(f"An unexpected error occured: {str(e)}")
+
+
 def read_dictionary(filename, key_column_index):
     """Read the contents of a CSV file into a compound
     dictionary and return the dictionary.
